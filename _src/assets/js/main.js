1658c5c42d023f048b722c1de6fac1f9dded2bda
superanika <anamaria.romerogarcia@gmail.com>
2019-05-03 18:08:36 +0200

    turn cards

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -5,10 +5,9 @@ const button = document.querySelector('.button');
 const radioButtons = document.querySelectorAll('.radio__button');
 let fullUrl;
 const resultsList = document.querySelector('.results__list');
-
 const getValue = event => {
   fullUrl = `${url}${event.currentTarget.value}.json`;
-};
+}
 
 function createCards(data) {
   for (const pokemon of data) {
@@ -16,15 +15,23 @@ function createCards(data) {
     listItem.classList.add('list__item');
     const imageFront = document.createElement('img');
     imageFront.src = pokemon.image;
-    imageFront.classList.add('image__front');
+    imageFront.classList.add('image__front', 'image', 'hidden');
     const imageBack = document.createElement('img');
     imageBack.src = 'assets/images/back.png';
-    imageBack.classList.add('image__back');
+    imageBack.classList.add('image__back', 'image');
     resultsList.appendChild(listItem);
     listItem.appendChild(imageFront);
     listItem.appendChild(imageBack);
+    listItem.addEventListener('click', turnCard);
   }
 }
+function turnCard(event) {
+  event.currentTarget.firstChild.classList.toggle('hidden');
+  event.currentTarget.lastChild.classList.toggle('hidden');
+
+}
+
+
 
 const startGame = () => {
   resultsList.innerHTML = '';
@@ -33,7 +40,8 @@ const startGame = () => {
     .then(data => {
       createCards(data);
     });
-};
+}
+
 
 
 for (const radioButton of radioButtons) {
