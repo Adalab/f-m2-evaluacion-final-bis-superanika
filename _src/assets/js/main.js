518c9853d9f9ec178e80efa634c95b7cb788654d
superanika <anamaria.romerogarcia@gmail.com>
2019-05-03 13:56:08 +0200

    create fetch

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
-console.log('>> Ready :)');
+const url = 'https://raw.githubusercontent.com/Adalab/cards-data/master/';
+const button = document.querySelector('.button');
+const radioButtons = document.querySelectorAll('.radio__button');
+let fullUrl;
+const resultsList = document.querySelector('.results__list');
+
+const getValue = event => {
+  fullUrl = `${url}${event.currentTarget.value}.json`;
+};
+
+function createCards(data) {
+  for (const pokemon of data) {
+    console.log(pokemon.image);
+  }
+}
+
+const startGame = () => {
+  fetch(fullUrl)
+    .then(response => response.json())
+    .then(data => {
+      createCards(data);
+    });
+};
+
+
+for (const radioButton of radioButtons) {
+  radioButton.addEventListener('click', getValue);
+}
+
+button.addEventListener('click', startGame);
